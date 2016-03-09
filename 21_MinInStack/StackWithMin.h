a9448af23d8ac025ace6f1936d4006e2b7243565
wangren <wangren725@163.com>
2016-03-09 19:05:45 +0800

    completed 21_MinInStack

--- 21_MinInStack/StackWithMin.h
+++ 21_MinInStack/StackWithMin.h
@@ -12,7 +12,7 @@
 using namespace std;
 
 template <typename T>
-class StackWithMin<T> {
+class StackWithMin {
 public:
     StackWithMin( int size = STACK_MAX_SIZE );
     ~StackWithMin() {};
@@ -29,6 +29,67 @@ private:
     int _capacity;
     int _size;
 };
+
+template <typename T>
+StackWithMin<T>::StackWithMin( int size )
+{
+    if ( size <= 0 )
+        throw "too small size.";
+
+    _capacity = size;
+    _size = 0;
+}
+
+template <typename T>
+void StackWithMin<T>::push( const T val )
+{
+    if ( IsFull() )
+        throw "Stack is full.";
+
+    _dataStack.push( val );
+
+    T temp;
+    if ( _size <= 0 )
+        temp = val;
+    else
+    {
+        temp = _minStack.top();
+        temp = val < temp ? val : temp;
+    }
+    _minStack.push( temp );
+
+    _size++;
+}
+
+template <typename T>
+void StackWithMin<T>::pop()
+{
+    if ( IsEmpty() )
+        throw "Stack is empty.";
+
+    _dataStack.pop();
+    _minStack.pop();
+    _size--;
+}
+
+template <typename T>
+const T& StackWithMin<T>::top() const
+{
+    if ( IsEmpty() )
+        throw "Stack is empty. ";
+
+    return _dataStack.top();
+}
+
+template <typename T>
+const T& StackWithMin<T>::min() const
+{
+    if ( IsEmpty() )
+        throw "Stack is Empty.";
+
+    return _minStack.top();
+}
+
 
 
 #endif //INC_21_MININSTACK_STACKWITHMIN_H
--- 21_MinInStack/main.cpp
+++ 21_MinInStack/main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include "StackWithMin.h"
 
 using namespace std;
 
+template <typename T>
+void checkStack( StackWithMin<T> stack )
+{
+    cout << "Top: " << stack.top() << endl;
+    cout << "Min: " << stack.min() << endl;
+
+    cout << endl;
+}
+
 int main() {
     cout << "Hello, World!" << endl;
+
+    int capacity = 3;
+    StackWithMin<int> stackWithMin( capacity );
+//    stackWithMin.top();
+    stackWithMin.push( 5 );
+    checkStack( stackWithMin );
+
+    stackWithMin.push( 6 );
+    checkStack( stackWithMin );
+
+    stackWithMin.push( 3 );
+    checkStack( stackWithMin );
+
+    stackWithMin.push( -1 );
+    checkStack( stackWithMin );
+
+    stackWithMin.pop();
+    checkStack( stackWithMin );
+
+    stackWithMin.pop();
+    checkStack( stackWithMin );
+
+    stackWithMin.pop();
+    checkStack( stackWithMin );
+
+    stackWithMin.pop();
+    checkStack( stackWithMin );
+
+    stackWithMin.pop();
+    checkStack( stackWithMin );
+
     return 0;
 }
